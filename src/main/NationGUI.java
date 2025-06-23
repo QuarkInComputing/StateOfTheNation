27c813892b761fe68913d76a5957c78acb5b934b
ThirdTitan <jackmcleanwilson@gmail.com>
2025-06-23 14:15:16 +0100

    Added test event

--- src/main/NationGUI.java
+++ src/main/NationGUI.java
@@ -37,6 +37,7 @@ public class NationGUI extends javax.swing.JFrame {
         jTextPane2 = new javax.swing.JTextPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1000, 800));
         setResizable(false);
         getContentPane().setLayout(new java.awt.CardLayout());
 
@@ -94,7 +95,7 @@ public class NationGUI extends javax.swing.JFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(331, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         getContentPane().add(pnl_countryinfo, "card2");
