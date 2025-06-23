2548195b9e6fc1f0ed08395543a58ac83f09a113
ThirdTitan <jackmcleanwilson@gmail.com>
2025-06-23 01:33:18 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+<div align="center">
+  <a href="https://github.com/othneildrew/Best-README-Template">
+    <img src="Icon.png" alt="Logo" width="80" height="80">
+  </a>
+</div>
+
+<h3 align="center">State Of The Nation</h3>
+
+# Overview
+A game made with the Java Swing framework, heavily inspired by the 
+(2002-Current) web game [NationStates](https://nationstates.net).
+
+## Features
+
+*Features list to be updated - upon project development*
+
+## Roadmap
+
+- [ ] Customisable nations
+- [ ] Pool of country issues to act on
+- [ ] War system
+- [ ] More to come
+
+## License
+
+Distributed under the MIT license. For more info see `LICENSE.txt`. 
---
+++ stateofthenation/Nation.java
@@ -0,0 +1,40 @@
+package stateofthenation;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Nation {
+    private BufferedImage flag;
+
+    //Constructor
+    Nation(){
+        setFlag();
+    }
+
+    //Methods
+    private void setFlag(){
+        try {
+            flag = ImageIO.read(new File("./src/assets/flag.png"));
+            BufferedImage scaledFlag = new BufferedImage(100, 60, BufferedImage.TYPE_INT_ARGB);
+
+            java.awt.Graphics2D g2d = scaledFlag.createGraphics();
+            g2d.drawImage(flag, 0, 0, 100, 60, null);
+            g2d.dispose();
+
+            flag = scaledFlag;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //Getters & Setters
+    public BufferedImage getFlag() {
+        return flag;
+    }
+
+    public void setFlagPath(BufferedImage flagPath) {
+        this.flag = flagPath;
+    }
+}
